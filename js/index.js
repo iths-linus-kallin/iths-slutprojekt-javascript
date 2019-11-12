3dfada57ca9bda7bf40a5720db6d1763604a956a
Linus Kallin <jaglinus@gmail.com>
2019-11-12 17:28:42 +0100

    Rytm 2

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ function piano(){
     var keyToElementBlack = { "w":".ciss1", "e":".diss1", "t":".fiss1", "y":".giss1", "u":".aiss1" }
     var elementToPitch = { ".c1":"C4", ".ciss1":"C#4", ".d1":"D4", ".diss1":"D#4", ".e1":"E4", ".f1":"F4", ".fiss1":"F#4", ".g1":"G4", ".giss1":"G#4", ".a1":"A4", ".aiss1":"A#4", ".b1":"B4", ".c2":"C5" }
 
-    var gain = new Tone.Gain(0.1)
+    var gain = new Tone.Gain(0.03)
     var freeverb = new Tone.Freeverb()
     freeverb.dampening.value = 2000;
     freeverb.roomSize.value = 0.87;
@@ -60,9 +60,9 @@ function piano(){
 // -----------------MOUSE CLICK-----------------
 
     let keys = document.querySelectorAll("section")
-    for(let key of keys){
+    for(let k of keys){
         addEventListener("mousedown", e => {
-            polySynth.triggerAttackRelease(elementToPitch[e.key])
+            polySynth.triggerAttackRelease(elementToPitch)
         })
         addEventListener("mouseup", e => {
             polySynth.triggerRelease()
@@ -79,13 +79,13 @@ function kick(){
     freeverb.roomSize.value = 0.5;
     freeverb.wet.value = 0.5
     var kick = new Tone.MembraneSynth()
-    kick.chain(gain, filter, freeverb, Tone.Master)
+    kick.chain(gain, filter, Tone.Master)
     var loop = new Tone.Loop(function(time){
         kick.triggerAttackRelease("C2", "4n");
     }, "4n").start(0);
     Tone.Transport.start();
     
-    let kickbutton = document.querySelector(".rythm")
+    let kickbutton = document.querySelector(".kick")
     kickbutton.addEventListener("click", e => {
         if(loop.state == "stopped"){
             loop.start(0);
@@ -98,20 +98,27 @@ function kick(){
 
 function snare(){
 
-    var gain = new Tone.Gain(0.1)
-    var filter = new Tone.Filter(5000, "lowpass", -12)
+    var gain = new Tone.Gain(0.05)
+    var dist = new Tone.Distortion(0.2)
     var freeverb = new Tone.Freeverb()
     freeverb.dampening.value = 1000;
     freeverb.roomSize.value = 0.5;
-    freeverb.wet.value = 0.5
-    var snare = new Tone.MembraneSynth()
-    snare.chain(gain, filter, freeverb, Tone.Master)
+    freeverb.wet.value = 0.3
+    var snare = new Tone.MembraneSynth({
+        envelope: {
+            attack: 0.001,
+            decay: 0.2,
+            sustain: 0,
+            release: 0.2
+        }
+      })
+    snare.chain(gain, freeverb, dist, Tone.Master)
     var loop = new Tone.Loop(function(time){
-        snare.triggerAttackRelease("C3", "4n");
+        snare.triggerAttackRelease("G3", "8n");
     }, "2n").start(0);
     Tone.Transport.start();
     
-    let kickbutton = document.querySelector(".rythm")
+    let kickbutton = document.querySelector(".snare")
     kickbutton.addEventListener("click", e => {
         if(loop.state == "stopped"){
             loop.start(0);
