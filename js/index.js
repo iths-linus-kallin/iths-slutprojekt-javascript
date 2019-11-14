0ff3250156f0289c43ca1f3b3b6109f364089000
Linus Kallin <jaglinus@gmail.com>
2019-11-14 14:18:40 +0100

    changeSound

--- js/index.js
+++ js/index.js
@@ -1,54 +1,55 @@
+var polySynth = new Tone.PolySynth(3, Tone.Synth)
+
 function piano(){
     
     var keyToPitch = { "a":"C4", "w":"C#4", "s":"D4", "e":"D#4", "d":"E4", "f":"F4", "t":"F#4", "g":"G4", "y":"G#4", "h":"A4", "u":"A#4", "j":"B4", "k":"C5" }
     var keyToElement = { "a":".c1", "w":".ciss1", "s":".d1", "e":".diss1", "d":".e1", "f":".f1", "t":".fiss1", "g":".g1", "y":".giss1", "h":".a1", "u":".aiss1", "j":".b1", "k":".c2" }
     var keyToElementBlack = { "w":".ciss1", "e":".diss1", "t":".fiss1", "y":".giss1", "u":".aiss1" }
-    var elementToPitch = { ".c1":"C4", ".ciss1":"C#4", ".d1":"D4", ".diss1":"D#4", ".e1":"E4", ".f1":"F4", ".fiss1":"F#4", ".g1":"G4", ".giss1":"G#4", ".a1":"A4", ".aiss1":"A#4", ".b1":"B4", ".c2":"C5" }
 
-    var gain = new Tone.Gain(0.03)
+    var gain = new Tone.Gain(0.05)
     var freeverb = new Tone.Freeverb()
     freeverb.dampening.value = 2000;
-    freeverb.roomSize.value = 0.87;
+    freeverb.roomSize.value = 0.8;
     freeverb.wet.value = 0.5
-    var filter = new Tone.Filter(1500, "lowpass", -12)
+    var filter = new Tone.Filter(3000, "lowpass", -12)
     var chorus = new Tone.Chorus(4, 2.5, 0.04)
-    var delay = new Tone.FeedbackDelay("8n", 0.3)
+    var delay = new Tone.FeedbackDelay("8n", 0.2)
 
-    var polySynth = new Tone.PolySynth(3, Tone.Synth)
     polySynth.set({
         oscillator: {
-          type: 'square',
+          type: 'sine',
         },
         envelope: {
           attack: 0.001,
-          decay: 19,
+          decay: 2,
           sustain: 0,
-          release: 5
+          release: 4
         }
-      })
+    })
     polySynth.chain(gain, filter, chorus, freeverb, delay, Tone.Master)
 
 // ----------------KEYBOARD--------------------
 
     var down = false;
     window.addEventListener('keydown', e => {
-        if(down) return;
-        down = true;
-        polySynth.triggerAttack(keyToPitch[e.key])
-        let keys = document.querySelector(keyToElement[e.key])
-        keys.style.background = "rgb(184, 55, 32)"
-    }, false)
+        if(Object.keys(keyToPitch).includes(e.key)){
+            polySynth.triggerAttack(keyToPitch[e.key])
+            let keysWhite = document.querySelector(keyToElement[e.key])
+            keysWhite.classList.add("key-white-push")
+            let keysBlack = document.querySelector(keyToElementBlack[e.key])
+            keysBlack.classList.add("key-black-push")
+        }else{
+            return
+        }
+    })
 
-    window.addEventListener('keyup', function () {
-        down = false;
-    }, false);   
-
+    
     window.addEventListener('keyup', e => {
         polySynth.triggerRelease(keyToPitch[e.key])
         let keysWhite = document.querySelector(keyToElement[e.key])
-        keysWhite.style.background = "ivory"
+        keysWhite.classList.remove("key-white-push")
         let keysBlack = document.querySelector(keyToElementBlack[e.key])
-        keysBlack.style.background = "black"
+        keysBlack.classList.remove("key-black-push")
     })
 
 // -----------------MOUSE CLICK-----------------
@@ -56,7 +57,8 @@ function piano(){
     let keys = document.querySelectorAll("section")
     for(var k = 0; k < keys.length; k++){
         keys[k].addEventListener("mousedown", e => {
-            polySynth.triggerAttackRelease(elementToPitch)
+            let tone = e.currentTarget.getAttribute("data-tone")
+            polySynth.triggerAttack(tone)
         })
         keys[k].addEventListener("mouseup", e => {
             polySynth.triggerRelease()
@@ -85,7 +87,7 @@ function kick(){
         if(loop.mute == true){
             loop.mute = false
             Tone.Transport.start();
-            kickbutton.style.backgroundImage = "url(https://www.dropbox.com/s/nc1aas1yk3o6tox/play.png?raw=1)"
+            kickbutton.style.backgroundImage = "url(https://www.dropbox.com/s/cxg7v6yrieajzdv/stop.png?raw=1)"
         }else{
             loop.mute = true
             kickbutton.style.backgroundImage = "url(https://www.dropbox.com/s/pi6ra58qvowk8l2/kick.png?raw=1)"
@@ -96,8 +98,8 @@ function kick(){
 
 function snare(){
 
-    var gain = new Tone.Gain(0.05)
-    var dist = new Tone.Distortion(0.2)
+    var gain = new Tone.Gain(0.03)
+    var dist = new Tone.Distortion(0.3)
     var freeverb = new Tone.Freeverb()
     freeverb.dampening.value = 1000;
     freeverb.roomSize.value = 0.5;
@@ -112,7 +114,7 @@ function snare(){
       })
     snare.chain(gain, freeverb, dist, Tone.Master)
     var loop = new Tone.Loop(function(time){
-        snare.triggerAttackRelease("G3", "8n");
+        snare.triggerAttackRelease("B3", "8n");
     }, "2n").start(0);
     loop.mute = true
     loop.start(0)
@@ -122,7 +124,7 @@ function snare(){
         if(loop.mute == true){
             loop.mute = false
             Tone.Transport.start();
-            snarebutton.style.backgroundImage = "url(https://www.dropbox.com/s/nc1aas1yk3o6tox/play.png?raw=1)"
+            snarebutton.style.backgroundImage = "url(https://www.dropbox.com/s/cxg7v6yrieajzdv/stop.png?raw=1)"
         }else{
             loop.mute = true
             snarebutton.style.backgroundImage = "url(https://www.dropbox.com/s/mkwz8ldzt2lvmtg/snare.png?raw=1)"
@@ -155,7 +157,7 @@ function hihat(){
         if(loop.mute == true){
             loop.mute = false
             Tone.Transport.start();
-            hihatButton.style.backgroundImage = "url(https://www.dropbox.com/s/nc1aas1yk3o6tox/play.png?raw=1)"
+            hihatButton.style.backgroundImage = "url(https://www.dropbox.com/s/cxg7v6yrieajzdv/stop.png?raw=1)"
         }else{
             loop.mute = true
             hihatButton.style.backgroundImage = "url(https://www.dropbox.com/s/4t8lwnhfa4bcfh2/hihat.png?raw=1)"
@@ -164,16 +166,49 @@ function hihat(){
     
 }
 
+function changeSound(){
+    var sound1 = document.querySelector(".sound1")
+    sound1.addEventListener('click', e=>{
+        polySynth.set({
+            oscillator: {
+              type: 'sine',
+            },
+            envelope: {
+              attack: 0.001,
+              decay: 19,
+              sustain: 0,
+              release: 5
+            }
+        })
+    })
+
+    var sound2 = document.querySelector(".sound2")
+    sound2.addEventListener('click', e=>{
+        polySynth.set({
+            oscillator: {
+              type: 'sawtooth',
+              detune: 5
+            },
+            envelope: {
+              attack: 0.001,
+              decay: 1,
+              sustain: 0,
+              release: 1
+            }
+        })
+    })
+}
 
 piano()
 kick()
 snare()
 hihat()
+changeSound()
 
-// let c = document.querySelector("body")
-// c.addEventListener("click", function(){
-//     if (Tone.context.state !== 'running') {
-//         Tone.context.resume();
-//       }
-//     piano()
-// })
+let c = document.querySelector("body")
+c.addEventListener("click", function(){
+    if (Tone.context.state !== 'running') {
+        Tone.context.resume();
+      }
+    piano()
+})
